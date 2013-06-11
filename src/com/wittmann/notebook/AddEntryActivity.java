a9428c7ade3415992160fd69df4697e05dc5ad05
DimaWittmann <dimawittmann@gmail.com>
2013-06-11 22:56:56 +0300

    Додані пікери дати і часу

--- src/com/wittmann/notebook/AddEntryActivity.java
+++ src/com/wittmann/notebook/AddEntryActivity.java
@@ -1,41 +1,55 @@
 package com.wittmann.notebook;
 
+import java.util.Calendar;
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class AddEntryActivity extends Activity
 							  implements OnClickListener{
 
 	private static final String TAG = AddEntryActivity.class.getSimpleName();
+	
 	EditText title;
-	EditText date;
-	EditText time;
+	Button date;
+	Button time;
 	EditText desc;
 	Button add;
+	
 	NotebookApplication app;
+	
+	Calendar dateAndTime = Calendar.getInstance();
+	
 	
 	public void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
 		 setContentView(R.layout.add);
 		 
 		 title = (EditText) findViewById(R.id.title);
-		 date = (EditText) findViewById(R.id.date);
-		 time = (EditText) findViewById(R.id.time);
+		 date = (Button) findViewById(R.id.date);
+		 time = (Button) findViewById(R.id.time);
 		 desc = (EditText) findViewById(R.id.desc);
+		 
+		 date.setOnClickListener(new DateButtonListener());
+		 time.setOnClickListener(new TimeButtonListener());
 		 
 		 add = (Button) findViewById(R.id.add);
 		 add.setOnClickListener(this);
@@ -55,8 +69,10 @@ public class AddEntryActivity extends Activity
 		SQLiteDatabase db = app.DBhelper.getWritableDatabase();
 		try{
 			db.insertOrThrow(DBHelper.TableName, null, cv);
+			Toast.makeText(getApplicationContext(), R.string.ToastAddEntry, Toast.LENGTH_SHORT).show();
 		}catch(SQLException e){
 			Log.e(TAG, e.getMessage());
+			Toast.makeText(getApplicationContext(), R.string.ToastAddEntryError, Toast.LENGTH_SHORT).show();
 		}finally{
 			db.close();
 		}
@@ -67,7 +83,7 @@ public class AddEntryActivity extends Activity
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.addactivitymenu, menu);
-		Log.d("AddAct", "menu");
+		
 		return true;	
 	}
 	
@@ -83,4 +99,57 @@ public class AddEntryActivity extends Activity
 		}
 		return true;
 	}
+	
+	
+	private class DateListener implements OnDateSetListener{
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			
+			Log.d(TAG, "DateListener");
+			dateAndTime.set(Calendar.YEAR, year);
+			dateAndTime.set(Calendar.MONTH, monthOfYear);
+			dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+			
+			String Date = String.format("%02d.%02d.%d", dayOfMonth, monthOfYear+1, year);
+			date.setText(Date);
+		}
+	}
+	
+	private class TimeListener implements OnTimeSetListener{
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			
+			Log.d(TAG, "TimeListener");
+			dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+			dateAndTime.set(Calendar.MINUTE, minute);
+			
+			String Time = String.format("%02d:%02d", hourOfDay, minute);
+			time.setText(Time);
+		}
+	}
+	
+	private class DateButtonListener implements OnClickListener{
+
+		@Override
+		public void onClick(View arg0) {
+			new DatePickerDialog(AddEntryActivity.this, new DateListener(), 
+					dateAndTime.get(Calendar.YEAR), dateAndTime.get(Calendar.MONTH), 
+					dateAndTime.get(Calendar.DAY_OF_MONTH)).show();
+		}
+		
+	}
+	
+	private class TimeButtonListener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			new TimePickerDialog(AddEntryActivity.this, new TimeListener(),
+					dateAndTime.get(Calendar.HOUR_OF_DAY), dateAndTime.get(Calendar.MINUTE), true).show();
+			
+		}
+		
+	}
+	
 }
